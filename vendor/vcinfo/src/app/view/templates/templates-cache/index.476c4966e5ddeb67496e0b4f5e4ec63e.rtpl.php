380f7942253e5f28488cd4338ddf79d4faaad416
VICTOR CAMPOS <victor-campos@outlook.com>
2018-06-27 23:43:20 -0400

    icon

--- vendor/vcinfo/src/app/view/templates/templates-cache/index.476c4966e5ddeb67496e0b4f5e4ec63e.rtpl.php
+++ vendor/vcinfo/src/app/view/templates/templates-cache/index.476c4966e5ddeb67496e0b4f5e4ec63e.rtpl.php
@@ -1,22 +1,47 @@
 <?php if(!class_exists('Rain\Tpl')){exit;}?><!DOCTYPE html>
 <html lang="pt-br">
-  <head>
+
+<head>
     <!-- Meta tags Obrigatórias -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://homepagina.com/dist/css/style.css">
+    <meta name="msapplication-config" content="http://homepagina.com/assets/img/favicons/browserconfig.xml" />
+    <link rel="apple-touch-icon" sizes="180x180" href="http://homepagina.com/assets/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="http://homepagina.com/assets/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="http://homepagina.com/assets/img/favicons/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="http://homepagina.com/assets/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="http://homepagina.com/assets/img/favicons/site.webmanifest">
+    <link rel="mask-icon" href="http://homepagina.com/assets/img/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-TileImage" content="http://homepagina.com/assets/img/favicons/mstile-144x144.png">
+    <meta name="theme-color" content="#ffffff">
 
     <title>V CAMPOS INFORMATICA : <?php echo htmlspecialchars( $title, ENT_COMPAT, 'UTF-8', FALSE ); ?></title>
-  </head>
-  <body>
-    <h1>Olá, mundo!</h1>
-
+</head>
+<body>
+    <div class="container">
+        <header>
+        </header>
+        <nav class="nav d-flex justify-content-between">
+          <a class="nav-link active" href="http://homepagina.com/home">Home</a>
+          <a class="nav-link" href="#">Active</a>
+          <a class="nav-link" href="#">Contato</a>
+        </nav>
+    </div>
+    <div  class="container">
+        <section></section>
+    </div>
+    <div class="container-fluid d_footer">
+        <footer></footer>
+    </div>
     <!-- JavaScript (Opcional) -->
     <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-  </body>
+</body>
 </html>
